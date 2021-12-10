2ebb28ea830b9b1c79aba6f238af6dcd4891108e
Joni Rajala <jonimatiasrajala@gmail.com>
2021-12-10 23:50:15 +0200

    UITimer is monospaced

--- Assets/scripts/UI/UITimerScifi.cs
+++ Assets/scripts/UI/UITimerScifi.cs
@@ -12,7 +12,7 @@ public class UITimerScifi : UITimerGeneric {
         int minutes = ((int)hp.time) / 60;
         int seconds = ((int)hp.time) % 60;
 
-        string timeStr = minutes + ":" + seconds;
+        string timeStr = "<mspace=0.5em>" + minutes + " " + seconds + "</mspace>";
 
         label.text = timeStr;
 
